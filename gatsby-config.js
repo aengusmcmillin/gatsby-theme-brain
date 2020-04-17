6e376f906e9ddac411aced8ffd3ffd367f76054c
Aengus McMillin <aengusmcmillin@gmail.com>
2020-04-16 18:05:24 -0700

    Add an mdxOtherwiseConfigured flag to work around a bug in gatsby-plugin-mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,12 @@ module.exports = ({
   noteTemplate = "./templates/brain.js", // Template to use for note rendering
   rootPath = "brain", // Set the root url for your site (e.g. in this case https://example.com/brain)
   rootNote = "brain", // Name of the note that will be used as the 'index' note. So in this case brain.md would generate the root page of the brain.
+  mdxOtherwiseConfigured = false, // Used to resolve a bug in gatsby-plugin-mdx
 }) => ({
-  plugins: ["gatsby-transformer-remark", "gatsby-plugin-mdx"],
+  plugins: [
+    !mdxOtherwiseConfigured && {
+      resolve: `gatsby-plugin-mdx`,
+    },
+    "gatsby-transformer-remark",
+  ].filter(Boolean),
 });
