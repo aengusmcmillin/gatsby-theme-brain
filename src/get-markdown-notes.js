8aae8d905325149e536328cd35add5772a367d34
Alex Dunne <hi@alexdunne.net>
2020-04-23 10:58:48 +0100

    Refactored getMarkdownNotes to remove intermediate variables

--- src/get-markdown-notes.js
+++ src/get-markdown-notes.js
@@ -4,21 +4,21 @@ const path = require("path");
 module.exports = (pluginOptions) => {
   let notesDirectory = pluginOptions.notesDirectory || "content/brain/";
 
-  let fileContents = [];
-
   let filenames = fs.readdirSync(notesDirectory);
-  filenames.forEach((filename) => {
+
+  return filenames.map((filename) => {
     let slug = pluginOptions.generateSlug
       ? pluginOptions.generateSlug(filename)
       : path.parse(filename).name.toLowerCase();
+
     let fullPath = notesDirectory + filename;
     let rawFile = fs.readFileSync(fullPath, "utf-8");
-    fileContents.push({
+
+    return {
       filename: filename,
       fullPath: fullPath,
       slug: slug,
       rawFile: rawFile,
-    });
+    };
   });
-  return fileContents;
 };
