93515c9c6602d2ab8f82412325e398132eed935d
Alex Dunne <hi@alexdunne.net>
2020-04-23 11:05:15 +0100

    Refactored insertLinks to use an if-guard approach to reduce nesting

--- src/insert-links.js
+++ src/insert-links.js
@@ -1,22 +1,32 @@
 const path = require("path");
 
 module.exports = (originalRawContent, nameToSlugMap, rootPath) => {
-  var newRawContent = originalRawContent;
+  let newRawContent = originalRawContent;
 
+  // Find matches for content between double brackets
+  // e.g. [[Example]] -> Example
   const regexExclusive = /(?<=\[\[).*?(?=\]\])/g;
+
+  // Find matches for content between double brackets including the brackets
+  // e.g. [[Example]] -> [[Example]]
   const regexInclusive = /\[\[.*?\]\]/g;
-  var replacementMatches = originalRawContent.match(regexInclusive);
-  if (replacementMatches != null) {
-    replacementMatches = replacementMatches.filter(
-      (a, b) => replacementMatches.indexOf(a) === b
-    );
-    replacementMatches.forEach((match) => {
-      var justText = match.match(regexExclusive)[0];
-      var link = nameToSlugMap[justText.toLowerCase()];
-      var linkPath = path.join("/", rootPath, link);
-      var linkified = `[${match}](${linkPath})`;
+
+  let replacementMatches = originalRawContent.match(regexInclusive);
+
+  if (replacementMatches === null) {
+    return newRawContent;
+  }
+
+  replacementMatches = replacementMatches
+    .filter((a, b) => replacementMatches.indexOf(a) === b)
+    .forEach((match) => {
+      let justText = match.match(regexExclusive)[0];
+      let link = nameToSlugMap[justText.toLowerCase()];
+      let linkPath = path.join("/", rootPath, link);
+      let linkified = `[${match}](${linkPath})`;
+
       newRawContent = newRawContent.split(match).join(linkified);
     });
-  }
+
   return newRawContent;
 };
