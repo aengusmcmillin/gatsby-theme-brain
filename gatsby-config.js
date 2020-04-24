ddd780ec4877979262a09bd6c72968eac7e8f15f
Alex Dunne <hi@alexdunne.net>
2020-04-24 08:31:37 +0100

    Added an option to configure which file extensions are used to generate notes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
 module.exports = ({
   notesDirectory = "content/brain/", // Directory containing your brain note files
+  notesFileExtensions = [".md", ".mdx"], // File extensions that will be used to generate pages
   noteTemplate = "./templates/brain.js", // Template to use for note rendering
   rootPath = "brain", // Set the root url for your site (e.g. in this case https://example.com/brain)
   rootNote = "brain", // Name of the note that will be used as the 'index' note. So in this case brain.md would generate the root page of the brain.
--- src/get-markdown-notes.js
+++ src/get-markdown-notes.js
@@ -3,10 +3,22 @@ const path = require("path");
 
 module.exports = (pluginOptions) => {
   let notesDirectory = pluginOptions.notesDirectory || "content/brain/";
+  let notesFileExtensions = pluginOptions.notesFileExtensions;
+
+  if (typeof notesFileExtensions === "undefined") {
+    notesFileExtensions = [".md", ".mdx"];
+  }
 
   let fileContents = [];
 
   let filenames = fs.readdirSync(notesDirectory);
+
+  if (notesFileExtensions) {
+    filenames = filenames.filter((filename) => {
+      return notesFileExtensions.includes(path.extname(filename).toLowerCase());
+    });
+  }
+
   filenames.forEach((filename) => {
     let slug = pluginOptions.generateSlug
       ? pluginOptions.generateSlug(filename)
