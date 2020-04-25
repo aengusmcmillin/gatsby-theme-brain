ce82913f4e3eb997f07a7e09e0ffa6eb89fc5a02
Alex Dunne <alexdunne@users.noreply.github.com>
2020-04-26 00:20:50 +0100

    Notes file extensions option (#14)
    * Added an option to configure which file extensions are used to generate notes
    
    * Set notesFileExtensions default value if supplied option is null or undefined

--- src/get-markdown-notes.js
+++ src/get-markdown-notes.js
@@ -3,22 +3,32 @@ const path = require("path");
 
 module.exports = (pluginOptions) => {
   let notesDirectory = pluginOptions.notesDirectory || "content/brain/";
+  let notesFileExtensions = pluginOptions.notesFileExtensions || [
+    ".md",
+    ".mdx",
+  ];
 
   let fileContents = [];
 
   let filenames = fs.readdirSync(notesDirectory);
-  filenames.forEach((filename) => {
-    let slug = pluginOptions.generateSlug
-      ? pluginOptions.generateSlug(filename)
-      : path.parse(filename).name.toLowerCase();
-    let fullPath = notesDirectory + filename;
-    let rawFile = fs.readFileSync(fullPath, "utf-8");
-    fileContents.push({
-      filename: filename,
-      fullPath: fullPath,
-      slug: slug,
-      rawFile: rawFile,
+
+  filenames
+    .filter((filename) => {
+      return notesFileExtensions.includes(path.extname(filename).toLowerCase());
+    })
+    .forEach((filename) => {
+      let slug = pluginOptions.generateSlug
+        ? pluginOptions.generateSlug(filename)
+        : path.parse(filename).name.toLowerCase();
+      let fullPath = notesDirectory + filename;
+      let rawFile = fs.readFileSync(fullPath, "utf-8");
+      fileContents.push({
+        filename: filename,
+        fullPath: fullPath,
+        slug: slug,
+        rawFile: rawFile,
+      });
     });
-  });
+
   return fileContents;
 };
