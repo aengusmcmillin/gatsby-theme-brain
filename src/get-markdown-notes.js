f158260d4687f76151525de280971fc63bb79231
Alex Dunne <hi@alexdunne.net>
2020-04-25 12:26:33 +0100

    Set notesFileExtensions default value if supplied option is null or undefined

--- src/get-markdown-notes.js
+++ src/get-markdown-notes.js
@@ -3,34 +3,32 @@ const path = require("path");
 
 module.exports = (pluginOptions) => {
   let notesDirectory = pluginOptions.notesDirectory || "content/brain/";
-  let notesFileExtensions = pluginOptions.notesFileExtensions;
-
-  if (typeof notesFileExtensions === "undefined") {
-    notesFileExtensions = [".md", ".mdx"];
-  }
+  let notesFileExtensions = pluginOptions.notesFileExtensions || [
+    ".md",
+    ".mdx",
+  ];
 
   let fileContents = [];
 
   let filenames = fs.readdirSync(notesDirectory);
 
-  if (notesFileExtensions) {
-    filenames = filenames.filter((filename) => {
+  filenames
+    .filter((filename) => {
       return notesFileExtensions.includes(path.extname(filename).toLowerCase());
+    })
+    .forEach((filename) => {
+      let slug = pluginOptions.generateSlug
+        ? pluginOptions.generateSlug(filename)
+        : path.parse(filename).name.toLowerCase();
+      let fullPath = notesDirectory + filename;
+      let rawFile = fs.readFileSync(fullPath, "utf-8");
+      fileContents.push({
+        filename: filename,
+        fullPath: fullPath,
+        slug: slug,
+        rawFile: rawFile,
+      });
     });
-  }
 
-  filenames.forEach((filename) => {
-    let slug = pluginOptions.generateSlug
-      ? pluginOptions.generateSlug(filename)
-      : path.parse(filename).name.toLowerCase();
-    let fullPath = notesDirectory + filename;
-    let rawFile = fs.readFileSync(fullPath, "utf-8");
-    fileContents.push({
-      filename: filename,
-      fullPath: fullPath,
-      slug: slug,
-      rawFile: rawFile,
-    });
-  });
   return fileContents;
 };
