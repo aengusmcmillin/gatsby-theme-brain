e5347b1ea9b3801595f9ae88dc6b6dd4b7db3430
Juan Caicedo <retiredcanadianpoet@gmail.com>
2020-06-16 13:01:15 -0700

    setup exclusion list (#33)

--- src/get-markdown-notes.js
+++ src/get-markdown-notes.js
@@ -1,12 +1,25 @@
 const fs = require("fs");
 const path = require("path");
 
+function toRegExp(value) {
+  if (typeof value === "string") {
+    return new RegExp(`^${value}$`);
+  }
+  return value;
+}
+
+const matches = (filename) => (regExp) => regExp.test(filename);
+const doesNotMatchAny = (regExps) => (filename) =>
+  !regExps.some(matches(filename));
+
 module.exports = (pluginOptions) => {
   let notesDirectory = pluginOptions.notesDirectory || "content/brain/";
   let notesFileExtensions = pluginOptions.notesFileExtensions || [
     ".md",
     ".mdx",
   ];
+  let exclusions =
+    (pluginOptions.exclude && pluginOptions.exclude.map(toRegExp)) || [];
 
   let filenames = fs.readdirSync(notesDirectory);
 
@@ -14,14 +27,16 @@ module.exports = (pluginOptions) => {
     .filter((filename) => {
       return notesFileExtensions.includes(path.extname(filename).toLowerCase());
     })
+    .filter(doesNotMatchAny(exclusions))
     .map((filename) => {
       let slug = pluginOptions.generateSlug
         ? pluginOptions.generateSlug(filename)
         : path.parse(filename).name.toLowerCase();
-  
+
       let fullPath = notesDirectory + filename;
+
       let rawFile = fs.readFileSync(fullPath, "utf-8");
-  
+
       return {
         filename: filename,
         fullPath: fullPath,
